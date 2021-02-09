fac43017e2cc01a1fc88d4b2153f17609b9093b2
ktmetcalfe <kmetcalfe4@gmail.com>
2021-02-09 17:21:40 -0500

    Added the ability for quotes and reading back info

--- BotTek.py
+++ BotTek.py
@@ -20,21 +20,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-# Does nothing if the message is from the bot itself
+    # Does nothing if the message is from the bot itself
     if message.author == client.user:
         return
 
-# Basic hello message and test point for the bot
+    # Basic hello message and test point for the bot
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-# Replies with an automated response when one of the (homo_words) are sent in a message
+    # Replies with an automated response when one of the (homo_words) are sent in a message
     if any(s in message.content for s in homo_words):
         await message.channel.send(f'{message.author.mention} No homo...')
 
 
-# Provides an automated response to anyone who starts their message with "@complaint",
-# also stores the complaint, minus "@complaint ", in the (filename_members) JSON file
+    # Provides an automated response to anyone who starts their message with "@complaint",
+    # also stores the complaint, minus "@complaint ", in the (filename_members) JSON file
     if message.content.startswith('@complaint'):
         complaint_json(message)
 
@@ -42,21 +42,32 @@ async def on_message(message):
         time.sleep(5)
         await message.channel.send(f'{message.author.mention} We are proccessing your feedback and will work swiftly to resolve your issue. We appreciate your patience.')
 
-# Provides an automated response to anyone who starts their message with "@complaint",
-# also stores the complaint, minus "@legal ", in the (filename_members) JSON file
+    # Provides an automated response to anyone who starts their message with "@complaint",
+    # also stores the complaint, minus "@legal ", in the (filename_members) JSON file
     if message.content.startswith('@legal'):
-        legal_json(message);
+        legal_json(message)
 
-    # Waits before sending message for dramatic effect
+        # Waits before sending message for dramatic effect
         time.sleep(5)
         await message.channel.send(f'{message.author.mention} You have the right to remain silent. Anything you say can and will be used against you in a court of law. You have the right to an attorney. If you cannot afford an attorney, one will be provided for you.')
 
+    if message.content.startswith('$quote'):
+        if message.content.startswith('$quotelist'):
+            await message.channel.send(quotelist_json(message))
+        else:
+            await message.channel.send(quote_json(message))
+
+    if message.content.startswith('$edits'):
+        await message.channel.send(edits_json(message))
+
 @client.event
 async def on_message_edit(messageOld, messageNew):
-# Counts the amount of times a member edits one of thier messages and adds it to
-# the (filename_members) JSON file
+    # Counts the amount of times a member edits one of thier messages and adds it to
+    # the (filename_members) JSON file
     if messageOld.content != messageNew.content:
         member = messageNew.author.name
+        member = member.lower()
+
         try:
             with open(filename_members, 'r') as data_file:
                 data_file.seek(0)
@@ -67,8 +78,8 @@ async def on_message_edit(messageOld, messageNew):
             print(f'New JSON: {data_string}')
             json_data = json.loads(data_string)
 
-    # Checks for existence of (member) and "edit_count" directories in the
-    # (filename_members) JSON file before editing and writing to the file
+        # Checks for existence of (member) and "edit_count" directories in the
+        # (filename_members) JSON file before editing and writing to the file
         if f'{member}' not in json_data:
             json_data[f'{member}'] = {}
             json_data[f'{member}']['edit_count'] = 1
@@ -83,7 +94,7 @@ async def on_message_edit(messageOld, messageNew):
 @client.event
 async def on_member_join(member):
     guild = member.guild
-# Send a welcoming message anytime someone joins the server
+    # Send a welcoming message anytime someone joins the server
     if guild.system_channel is not None:
         to_send = 'Welcome {0.mention} to {1.name}! I hope you enjoy your stay more than I do!'.format(member, guild)
         await guild.system_channel.send(to_send)
@@ -93,6 +104,8 @@ async def on_member_join(member):
 # Method for reading and writing complaints to the JSON file
 def complaint_json(message):
     member = message.author.name
+    member = member.lower()
+
     try:
         with open(filename_members, "r") as data_file:
             data_file.seek(0)
@@ -103,8 +116,8 @@ def complaint_json(message):
         print(f'New JSON: {data_string}')
         json_data = json.loads(data_string)
 
-# Checks for existence of (member) and "legal_issues" directories in the
-# (filename_members) JSON file before editing and writing to the file
+    # Checks for existence of (member) and "legal_issues" directories in the
+    # (filename_members) JSON file before editing and writing to the file
     if f'{member}' not in json_data:
         json_data[f'{member}'] = {}
         json_data[f'{member}']['complaints'] = []
@@ -119,6 +132,8 @@ def complaint_json(message):
 # Method for reading and writing legal issues to the JSON file
 def legal_json(message):
     member = message.author.name
+    member = member.lower()
+
     try:
         with open(filename_members, "r") as data_file:
             data_file.seek(0)
@@ -129,8 +144,8 @@ def legal_json(message):
         print(f'New JSON: {data_string}')
         json_data = json.loads(data_string)
 
-# Checks for existence of (member) and "legal_issues" directories in the
-# (filename_members) JSON file before editing and writing to the file
+    # Checks for existence of (member) and "legal_issues" directories in the
+    # (filename_members) JSON file before editing and writing to the file
     if f'{member}' not in json_data:
         json_data[f'{member}'] = {}
         json_data[f'{member}']['legal_issues'] = []
@@ -141,6 +156,78 @@ def legal_json(message):
 
     with open(filename_members, 'w') as data_file:
         json.dump(json_data, data_file, indent = 4)
+
+# Method for reading quotes from the JSON file
+def quotelist_json(message):
+    member = message.content.replace('$quotelist ', '')
+    member = member.lower()
+
+    try:
+        with open(filename_members, "r") as data_file:
+            data_file.seek(0)
+            json_data = json.load(data_file)
+    except:
+        print(f'{filename_members} does not exist or is inproperly formatted')
+        return 'No quotes exist'
+
+    # Checks for existence of (member) and "quotes" directories in the
+    # (filename_members) JSON file before reading the quotes
+    if f'{member}' not in json_data or 'quotes' not in json_data[f'{member}']:
+        return 'No quotes exist for this member'
+    else:
+        return json_data[member]["quotes"]
+
+# Method for writing quotes to the JSON file
+def quote_json(message):
+    content = message.content.replace('$quote ', '')
+    memberRaw = content[:content.index(' ')]
+    member = memberRaw.lower()
+    quote = content.replace(f'{memberRaw} ', '')
+
+    try:
+        with open(filename_members, "r") as data_file:
+            data_file.seek(0)
+            json_data = json.load(data_file)
+    except:
+        print(f'{filename_members} does not exist or is inproperly formatted')
+        data_string = f'{{"{member}": {{"quotes": []}}}}'
+        print(f'New JSON: {data_string}')
+        json_data = json.loads(data_string)
+
+    # Checks for existence of (member) and "quotes" directories in the
+    # (filename_members) JSON file before editing and writing to the file
+    if f'{member}' not in json_data:
+        json_data[f'{member}'] = {}
+        json_data[f'{member}']['quotes'] = []
+    elif 'quotes' not in json_data[f'{member}']:
+        json_data[f'{member}']['quotes'] = []
+
+    json_data[member]['quotes'].append(quote)
+
+    with open(filename_members, 'w') as data_file:
+        json.dump(json_data, data_file, indent = 4)
+
+    return f"Added quote to {memberRaw}'s list"
+
+ # Method for reading quotes from the JSON file
+def edits_json(message):
+    memberRaw = message.content.replace('$edits ', '')
+    member = memberRaw.lower()
+
+    try:
+        with open(filename_members, "r") as data_file:
+            data_file.seek(0)
+            json_data = json.load(data_file)
+    except:
+        print(f'{filename_members} does not exist or is inproperly formatted')
+        return 'No edits have been made'
+
+    # Checks for existence of (member) and "edit_count" directories in the
+    # (filename_members) JSON file before reading the edit_count
+    if f'{member}' not in json_data or 'edit_count' not in json_data[f'{member}']:
+        return f'{memberRaw} has not made an edits'
+    else:
+        return f" {memberRaw} has made {json_data[member]['edit_count']} edits"
 
 with open(filename_token) as token:
     client.run(token.read())
